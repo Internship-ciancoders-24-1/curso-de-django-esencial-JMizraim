b018bd1e728cde2ac22db16803dbff1b1fa2e401
JMizraim <jomizraim47@gmail.com>
2024-03-06 13:46:35 -0600

    Día-3, capítulo 3

--- practice/platzigram/platzigram/urls.py
+++ practice/platzigram/platzigram/urls.py
@@ -14,15 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from platzigram.views import hello_wold, sorted, say_hi
 from posts import views as posts_views
-
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('hello-world/', hello_wold),
     path('sorted', sorted),
     path('hi/<str:name>/<int:age>/', say_hi),
     path('posts/', posts_views.list_posts)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- practice/platzigram/posts/admin.py
+++ practice/platzigram/posts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from posts.models import Post
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'title', 'photo')
+    list_display_links = ('id', 'user')
+    list_editable = ('title', 'photo')
+    search_fields = (
+        'user__username',
+        'user__email',
+        'title'
+    )
+    list_filter = (
+        'created',
+        'modified'
+    )
+
+    fieldsets = (
+        ('Post', {
+            'fields': (('user', 'title', 'photo'),)
+        }),
+        ('Metadata', {
+            'fields': (('created', 'modified'),)
+        })
+    )
+
+    readonly_fields = ('created', 'modified')
